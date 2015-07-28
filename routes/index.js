55d573f0b7ab6008dee4d0bde421a43cd9056d44
Esskov Andrey <p_uzo@mail.ru>
2015-07-28 16:06:34 +0300

    put new user into db

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ module.exports = function (app) {
     app.get('/', function(req, res) {
         res.render('index');
     });
+    app.get('/registration', function(req, res) {
+        res.render('registration');
+    });
     //app.post('/registration', require('./registration').post);
+
+    app.post('/registration', function (req, res) {
+        console.log(req.body);
+        require('./registration').validDate(req.body.user, req.body.pass);
+        //res.redirect('/');
+    });
 
  };
 
