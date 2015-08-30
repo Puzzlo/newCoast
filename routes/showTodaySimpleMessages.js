615919e75413a4c6d5a8d850555156b24c3be6b2
Puzzlo <p_uzo@mail.ru>
2015-08-30 20:42:43 +0300

    begin history messages in chat windows

--- routes/showTodaySimpleMessages.js
+++ routes/showTodaySimpleMessages.js
@@ -4,6 +4,18 @@ var   config = require('config')
     , mongo = require('mongodb').MongoClient;
 
 var showTodaySimpleMessages = function () {
+
+    var now = new Date();
+    var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime().toString();
+    console.log(today);
+
+    mongo.connect(config.get('mongodb:uri') + config.get('mongodb:dbName'), function(err, db) {
+        if (err) throw err;
+        var col = db.collection(config.get('mongodb:history'));
+
+    });
+
+
     var res = { message: "1: bla-bla-bla"};
     return res;
 };
