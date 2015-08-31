734b8c0bc353b7dd1df57b692152c0d85f044dd4
Puzzlo <p_uzo@mail.ru>
2015-08-31 21:26:44 +0300

    we know where need to send confirm and not send

--- routes/showTodayConfirmMessages.js
+++ routes/showTodayConfirmMessages.js
@@ -11,16 +11,22 @@ var showTodayConfirmMessages = function (name) {
 
     mongo.connect(config.get('mongodb:uri') + config.get('mongodb:dbName'), function(err, db) {
         if (err) throw err;
-        var mess = {};
         var col = db.collection(config.get('mongodb:history'));
         col.find({_id: {$gt: midnight}}).forEach(function (res) {
+            console.log(res.confirmed || 'none');
             if(res.priv.length == 0 && res.confirm.length > 0) {
-                if(res.confirmed === undefined || res.confirmed.indexOf(name) == -1) {
-                    mess[res._id] = 1; // need to confirmed
-                } else {
-                    mess[res._id] = 0; // deja confirmed
+                var mess = {};
+                console.log('res.priv.length = ' + res.priv.length + '&& res.confirm.length = ' + res.confirm.length);
+                console.log('res.confirmed = ' + res.confirmed);
+                //+ ' && res.confirmed.indexOf(name) = ' + res.confirmed.indexOf(name));
+                if(res.confirm.indexOf(name)!= -1) {
+                    if(res.confirmed === undefined || res.confirmed.indexOf(name) == -1) {
+                        mess[res._id] = 1; // need to confirmed
+                    } else {
+                        mess[res._id] = 0; // deja confirmed
+                    }
+                    arrayOfMessages.push(mess);
                 }
-                arrayOfMessages.push(mess);
                 console.log('arr = ' + JSON.stringify(arrayOfMessages));
             }
         });
